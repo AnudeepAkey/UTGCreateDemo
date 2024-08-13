e895780f1d19a1901894698aa4a5c27cbe956d75
anudeep.akey <anudeep.akey@servicenow.com>
2024-08-13 14:20:11 -0400

    This is a Tezt commit for UTG

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 class Test
 {
-    public static void main(String []args)
-    {
-        System.out.println("My First Java Program.");
+    // Method to add two numbers
+    public static int addNumbers(int num1, int num2) {
+        return num1 + num2;
     }
-};
+
+    public static void main(String[] args) {
+        // Declare and initialize two variables
+        int number1 = 10;
+        int number2 = 20;
+
+        // Call the addNumbers method and store the result
+        int sum = addNumbers(number1, number2);
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
