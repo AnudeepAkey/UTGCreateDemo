42dd156a5592335e370645fc828f0cb4f911fc89
anudeep.akey <anudeep.akey@servicenow.com>
2024-08-14 14:07:59 -0400

    This is a Test commit for UTG with no indentation

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 class Test
 {
-    // Method to add two numbers
-    public static int addNumbers(int num1, int num2) {
-        return num1 - num2;
-    }
+// Method to add two numbers
+public static int addNumbers(int num1, int num2) {
+return num1 - num2;
+}
 
-    public static void main(String[] args) {
-        // Declare and initialize two variables
-        int number1 = 10;
-        int number2 = 20;
+public static void main(String[] args) {
+// Declare and initialize two variables
+int number1 = 10;
+int number2 = 20;
 
-        // Call the addNumbers method and store the result
-        int sum = addNumbers(number1, number2);
+// Call the addNumbers method and store the result
+int sum = addNumbers(number1, number2);
 
-        // Print the result
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
-    }
+// Print the result
+System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+}
